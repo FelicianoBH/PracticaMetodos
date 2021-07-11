41d0c2e5948548f293384a26d6b9d4d3c6b44d42
Feliciano <restauranteacaymo@gmail.com>
2021-07-11 10:42:35 +0100

    Agregar archivos de proyecto.

---
+++ PracticaMetodos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            mensajeEnPantalla();
+        }
+
+        static void mensajeEnPantalla()
+        {
+            Console.WriteLine("msg desde el´método");
+        }
+    }
+}
